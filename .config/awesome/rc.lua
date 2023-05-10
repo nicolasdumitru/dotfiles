892510cb6b73b9d5201595c469a310e5ff78d4e3
nicolasdumitru <nicolasdumitru13@gmail.com>
2023-05-11 00:30:43 +0300

    inconsequential

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -215,7 +215,6 @@ end)
 -- }}}
 
 -- {{{ Key bindings
-
 globalkeys = gears.table.join(
     awful.key({ modkey,           }, "s",      hotkeys_popup.show_help,
               {description="show help", group="awesome"}),
