767f0d15e1d5690b5fe61f7ccf686c8c1abedeb9
nicolasdumitru <nicolasdumitru13@gmail.com>
2023-05-12 22:06:38 +0300

    authentication agent

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -744,3 +744,6 @@ awful.spawn.with_shell("/usr/bin/nm-applet &")
 
 --Keyboard layout (with Xmodmap):
 awful.spawn.with_shell("/usr/bin/xmodmap ~/.config/xmodmap")
+
+--Authentication agent
+awful.spawn.with_shell("/usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 &")
