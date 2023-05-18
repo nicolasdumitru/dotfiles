b9a6e9381ab5860a8a5a1c378ccae21e083a8630
nicolasdumitru <nicolasdumitru13@gmail.com>
2023-05-18 19:00:05 +0300

    removed comment

--- .config/nvim/lua/plugin.lua
+++ .config/nvim/lua/plugin.lua
@@ -17,12 +17,10 @@ vim.opt.rtp:prepend(lazypath)
 -- Lazy plugins
 require("lazy").setup({
 	{ "ellisonleao/gruvbox.nvim", priority = 1000 },
-	{
-    'nvim-telescope/telescope.nvim', tag = '0.1.1',
--- or                              , branch = '0.1.1',
-      dependencies = { 'nvim-lua/plenary.nvim' }
+	{ "nvim-telescope/telescope.nvim", branch = '0.1.1',
+      dependencies = { "nvim-lua/plenary.nvim" }
   },
-	{"nvim-treesitter/nvim-treesitter", build = ":TSUpdate"}
+	{ "nvim-treesitter/nvim-treesitter", build = ":TSUpdate" }
 })
 
 -- Gruvbox theme options
