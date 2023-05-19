34c77bd85deb71f1a6ef254972be3d533f6519cf
nicolasdumitru <nicolasdumitru13@gmail.com>
2023-05-19 16:05:10 +0300

    spaces -> tabs; apostrophes -> quotes

--- .config/nvim/lua/plugin.lua
+++ .config/nvim/lua/plugin.lua
@@ -3,14 +3,14 @@
 -- Lazy plugin manager
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
-  })
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable", -- latest stable release
+		lazypath,
+	})
 end
 vim.opt.rtp:prepend(lazypath)
 
@@ -19,28 +19,28 @@ require("lazy").setup({
 	{ "ellisonleao/gruvbox.nvim", priority = 1000 },
 	{ "nvim-treesitter/nvim-treesitter", build = ":TSUpdate" },
 	{
-    'VonHeikemen/lsp-zero.nvim',
-    branch = 'v2.x',
-    dependencies = {
-      -- LSP Support
-      {'neovim/nvim-lspconfig'},             -- Required
-      {                                      -- Optional
-        'williamboman/mason.nvim',
-        build = function()
-          pcall(vim.cmd, 'MasonUpdate')
-        end,
-      },
-      {'williamboman/mason-lspconfig.nvim'}, -- Optional
+		"VonHeikemen/lsp-zero.nvim",
+		branch = "v2.x",
+		dependencies = {
+			-- LSP Support
+			{"neovim/nvim-lspconfig"},             -- Required
+			{                                      -- Optional
+				"williamboman/mason.nvim",
+				build = function()
+				  pcall(vim.cmd, "MasonUpdate")
+				end,
+			},
+			{"williamboman/mason-lspconfig.nvim"}, -- Optional
 
-      -- Autocompletion
-      {'hrsh7th/nvim-cmp'},     -- Required
-      {'hrsh7th/cmp-nvim-lsp'}, -- Required
-      {'L3MON4D3/LuaSnip'},     -- Required
+			-- Autocompletion
+			{"hrsh7th/nvim-cmp"},     -- Required
+			{"hrsh7th/cmp-nvim-lsp"}, -- Required
+			{"L3MON4D3/LuaSnip"},     -- Required
 		}
 	},
 	{
-		"nvim-telescope/telescope.nvim", branch = '0.1.x',
-      dependencies = { "nvim-lua/plenary.nvim" }
+		"nvim-telescope/telescope.nvim", branch = "0.1.x",
+			dependencies = { "nvim-lua/plenary.nvim" }
 	}
 })
 
@@ -53,18 +53,6 @@ vim.cmd("colorscheme gruvbox")
 
 -- LSP (LSP Zero)
 require("pluginconfig.lsp-zero-config")
---[[
-local lsp = require('lsp-zero').preset({})
-
-lsp.on_attach(function(client, bufnr)
-  lsp.default_keymaps({buffer = bufnr})
-end)
-
-require('lspconfig').lua_ls.setup(lsp.nvim_lua_ls())
-
-lsp.setup()
-
-]]--
 
 -- Treesitter setup
 require("pluginconfig.treesitter-setup")
