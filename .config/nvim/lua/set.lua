8853228bc5e8764dbc89d22cc4b7a6229caf8ff9
nicolasdumitru <nicolasdumitru13@gmail.com>
2023-06-17 14:58:05 +0300

    4 space tabs

--- .config/nvim/lua/set.lua
+++ .config/nvim/lua/set.lua
@@ -3,8 +3,8 @@ vim.opt.number = true
 vim.opt.relativenumber = true
 
 -- Indentation settings
-vim.opt.shiftwidth = 2
-vim.opt.tabstop = 2
+vim.opt.shiftwidth = 4
+vim.opt.tabstop = 4
 vim.opt.smartindent = true
 vim.opt.expandtab = false
 
