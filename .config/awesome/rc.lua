e269326b8265bd87d2c239bdbcd0bca9cb9f32e7
nicolasdumitru <nicolasdumitru13@gmail.com>
2023-10-17 21:16:40 +0300

    awesomewm - layouts

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -101,9 +101,8 @@ Modkey2 = "Mod1"
 -- Table of layouts to cover with awful.layout.inc, order matters.
 awful.layout.layouts = {
 	awful.layout.suit.tile,
-	awful.layout.suit.tile.bottom,
-	awful.layout.suit.tile.top,
-	--awful.layout.suit.floating,
+	awful.layout.suit.tile.left,
+	awful.layout.suit.max,
 }
 -- }}}
 
