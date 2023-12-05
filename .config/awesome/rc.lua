2605c0abe1e8c1dece07b9d2b125202b20c68043
nicolasdumitru <nicolasdumitru13@gmail.com>
2023-12-05 21:36:07 +0200

    awesomewm - removed fallback config

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -16,33 +16,6 @@ local hotkeys_popup = require("awful.hotkeys_popup")
 -- when client with a matching name is opened:
 require("awful.hotkeys_popup.keys")
 
--- {{{ Error handling Check if awesome encountered an error during startup and fell back to another config (This code will only ever execute for the fallback config)
-if awesome.startup_errors then
-	naughty.notify({
-		preset = naughty.config.presets.critical,
-		title = "Oops, there were errors during startup!",
-		text = awesome.startup_errors
-	})
-end
-
--- Handle runtime errors after startup
-do
-	local in_error = false
-	awesome.connect_signal("debug::error", function(err)
-		-- Make sure we don't go into an endless error loop
-		if in_error then return end
-		in_error = true
-
-		naughty.notify({
-			preset = naughty.config.presets.critical,
-			title = "Oops, an error happened!",
-			text = tostring(err)
-		})
-		in_error = false
-	end)
-end
--- }}}
-
 -- {{{ Variable definitions
 -- Themes define colours, icons, font and wallpapers.
 beautiful.init(gears.filesystem.get_configuration_dir() .. "themes/gruvbox.lua")
