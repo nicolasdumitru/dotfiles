f7fff26189aae443224bf6235035abb8f2d2f59d
nicolasdumitru <nicolasdumitru13@gmail.com>
2024-04-18 20:11:15 +0300

    .xprofile -> xautostart

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -836,3 +836,6 @@ end)
 client.connect_signal("focus", function(c) c.border_color = beautiful.border_focus end)
 client.connect_signal("unfocus", function(c) c.border_color = beautiful.border_normal end)
 -- }}}
+
+-- Automatically start programs
+awful.spawn.with_shell("$XDG_CONFIG_HOME/autostart/xautostart")
